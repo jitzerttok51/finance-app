9ba797d87143b4c9628fcb709bc24a40041b7a06
Nikolay Prodanov <Nikolay.Prodanov@proton.me>
2025-02-20 08:59:18 +0200

    Added Quaser Framework

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { Quasar } from "quasar";
 
-createApp(App).mount("#app");
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/dist/quasar.css'
+
+createApp(App)
+.use(Quasar)
+.mount("#app");
